0b6868a9df7a91b692fb202bf88cb1b1ae1a7347
Brody14 <stefania.spinas@gmail.com>
2023-04-12 16:59:04 +0200

    FEAT print trains

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function homepage()
+    {
+        $trains = Train::whereDate('departure_date', '>=', date('Y-m-d'))->get();
+
+        $data = [
+            'trains' => $trains
+        ];
+        return view('welcome', $data);
+    }
+}
